4db779fd7a71299f3e46ee00b3428d5bb601d731
tiennt0212@gmail.com <leulue@example.com>
2021-06-06 01:22:40 -0700

    Tien thu lam 2 button

--- packages/ec-web/components/Button/button.stories.js
+++ packages/ec-web/components/Button/button.stories.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Button} from './Button';
+import { Button } from './button';
 
 export default {
-  title: 'Example/Button',
+  title: 'Components/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -15,22 +15,11 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Primary Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+  primary: false,
+  label: 'Secondary Button',
 };
